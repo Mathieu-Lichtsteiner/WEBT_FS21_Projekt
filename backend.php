dcfc637efe0f347fe8fc57be714fa9a97dc8b474
Mathieu Lichtsteiner <Mathieu-Lichtsteiner@users.noreply.github.com>
2021-05-22 22:52:54 +0200

    inserted Checkboxes to the Tasklist and protected it.

--- backend.php
+++ backend.php
@@ -2,7 +2,14 @@
 <html>
 
 <head>
-
+	<meta charset="UTF-8"/>
+	<meta name="author" content="Mathieu Lichtsteiner"/>
+	<meta name="description" content="Das ist das Resultat des Unterrichts im Modul \&quotWebtechnologien\&quot. Diese Webseite ist mit Humor zu geniessen..."/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<title>
+		Mathieu's Makeup Studio
+	</title>
+	<link rel="shortcut icon" type="image/png" href="img/lipstick.png"/>
 </head>
 
 <body>
