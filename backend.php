d3d21983c0a930023d1dac803bceb5bc75d95f79
Mathieu Lichtsteiner <Mathieu-Lichtsteiner@users.noreply.github.com>
2021-05-23 14:56:35 +0200

    Unified naming-convention.

--- backend.php
+++ backend.php
@@ -7,7 +7,7 @@
 	<meta name="description" content="Das ist das Resultat des Unterrichts im Modul \&quot;Webtechnologien\&quot;. Diese Webseite ist mit Humor zu geniessen..."/>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<title>
-		Mathieu's Makeup Studio
+		Mathieu's MakeUp Studio
 	</title>
 
 	<link rel="shortcut icon" type="image/png" href="img/lipstick.png"/>
@@ -19,13 +19,13 @@
 
 	<nav class="w3-bar">
 		<a class="w3-bar-item w3-button w3-mobile w3-hide-medium w3-hide-large">&equiv;</a><!-- In den Unterlage &#2261, funktioniert aber in utf8 nicht!, mit &#8801 würde es gehen, aber die html-identity gefällt mir am besten. -->
-		<a href="./index.html#Home" class="w3-bar-item w3-button w3-mobile w3-hide-small">Home</a>
-		<a href="./index.html#Informationen" class="w3-bar-item w3-button w3-mobile w3-hide-small">Informationen</a>
-		<a href="./index.html#Zeichnen" class="w3-bar-item w3-button w3-mobile w3-hide-small">Zeichnen!</a>
-		<a href="./index.html#Beitragen" class="w3-bar-item w3-button w3-mobile w3-hide-small">Beitragen!</a>
-		<a href="./index.html#Posts" class="w3-bar-item w3-button w3-mobile w3-hide-small">Posts ansehen!</a>
+		<a href="./index.html#home" class="w3-bar-item w3-button w3-mobile w3-hide-small">Home</a>
+		<a href="./index.html#information" class="w3-bar-item w3-button w3-mobile w3-hide-small">Informationen</a>
+		<a href="./index.html#draw" class="w3-bar-item w3-button w3-mobile w3-hide-small">Zeichnen!</a>
+		<a href="./index.html#submit" class="w3-bar-item w3-button w3-mobile w3-hide-small">Beitragen!</a>
+		<a href="./index.html#posts" class="w3-bar-item w3-button w3-mobile w3-hide-small">Posts ansehen!</a>
 	</nav>
-	
+
 </body>
 
 </html>
