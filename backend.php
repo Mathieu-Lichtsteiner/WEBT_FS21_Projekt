0c6be71bc69526258230e394d9dd1937c45f105b
Mathieu Lichtsteiner <Mathieu-Lichtsteiner@users.noreply.github.com>
2021-05-23 12:37:53 +0200

    Added link to the W3-CSS Library. Added the Nav-Bar (styled with w3). Added the language Attribute.

--- backend.php
+++ backend.php
@@ -1,19 +1,27 @@
 <!DOCTYPE html>
-<html>
+<html lang="de">
 
 <head>
 	<meta charset="UTF-8"/>
 	<meta name="author" content="Mathieu Lichtsteiner"/>
-	<meta name="description" content="Das ist das Resultat des Unterrichts im Modul \&quotWebtechnologien\&quot. Diese Webseite ist mit Humor zu geniessen..."/>
+	<meta name="description" content="Das ist das Resultat des Unterrichts im Modul \&quot;Webtechnologien\&quot;. Diese Webseite ist mit Humor zu geniessen..."/>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<title>
 		Mathieu's Makeup Studio
 	</title>
+
 	<link rel="shortcut icon" type="image/png" href="img/lipstick.png"/>
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"/>
 </head>
 
 <body>
-
+	<nav class="w3-bar">
+		<a class="w3-bar-item w3-button w3-mobile w3-hide-medium w3-hide-large">&equiv;</a><!-- In den Unterlage &#2261, funktioniert aber in utf8 nicht!, mit &#8801 würde es gehen, aber die html-identity gefällt mir am besten. -->
+		<a href="./index.html#Informationen" class="w3-bar-item w3-button w3-mobile w3-hide-small">Informationen</a>
+		<a href="./index.html#Zeichnen" class="w3-bar-item w3-button w3-mobile w3-hide-small">Zeichnen!</a>
+		<a href="./index.html#Beitragen" class="w3-bar-item w3-button w3-mobile w3-hide-small">Beitragen!</a>
+		<a href="./index.html#Posts" class="w3-bar-item w3-button w3-mobile w3-hide-small">Posts ansehen!</a>
+	</nav>
 </body>
 
 </html>
