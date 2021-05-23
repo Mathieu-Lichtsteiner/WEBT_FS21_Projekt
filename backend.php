c985508c6268fe1285a5718c660645c58f462afe
Mathieu Lichtsteiner <Mathieu-Lichtsteiner@users.noreply.github.com>
2021-05-23 14:46:29 +0200

    Added the main CSS file. Added the 'home' section.

--- backend.php
+++ backend.php
@@ -12,16 +12,20 @@
 
 	<link rel="shortcut icon" type="image/png" href="img/lipstick.png"/>
 	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"/>
+	<link rel="stylesheet" href="css/main.css"/>
 </head>
 
 <body>
+
 	<nav class="w3-bar">
 		<a class="w3-bar-item w3-button w3-mobile w3-hide-medium w3-hide-large">&equiv;</a><!-- In den Unterlage &#2261, funktioniert aber in utf8 nicht!, mit &#8801 würde es gehen, aber die html-identity gefällt mir am besten. -->
+		<a href="./index.html#Home" class="w3-bar-item w3-button w3-mobile w3-hide-small">Home</a>
 		<a href="./index.html#Informationen" class="w3-bar-item w3-button w3-mobile w3-hide-small">Informationen</a>
 		<a href="./index.html#Zeichnen" class="w3-bar-item w3-button w3-mobile w3-hide-small">Zeichnen!</a>
 		<a href="./index.html#Beitragen" class="w3-bar-item w3-button w3-mobile w3-hide-small">Beitragen!</a>
 		<a href="./index.html#Posts" class="w3-bar-item w3-button w3-mobile w3-hide-small">Posts ansehen!</a>
 	</nav>
+	
 </body>
 
 </html>
