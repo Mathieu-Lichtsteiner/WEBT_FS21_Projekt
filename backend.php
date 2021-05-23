2e16219dd76888335dd17da5c2fbd2dbd5a66101
Mathieu Lichtsteiner <Mathieu-Lichtsteiner@users.noreply.github.com>
2021-05-23 16:30:16 +0200

    Basic Buttons for the Canvas Elements

--- backend.php
+++ backend.php
@@ -2,23 +2,24 @@
 <html lang="de">
 
 <head>
-	<meta charset="UTF-8"/>
-	<meta name="author" content="Mathieu Lichtsteiner"/>
-	<meta name="description" content="Das ist das Resultat des Unterrichts im Modul \&quot;Webtechnologien\&quot;. Diese Webseite ist mit Humor zu geniessen..."/>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<meta charset="UTF-8" />
+	<meta name="author" content="Mathieu Lichtsteiner" />
+	<meta name="description" content="Das ist das Resultat des Unterrichts im Modul \&quot;Webtechnologien\&quot;. Diese Webseite ist mit Humor zu geniessen..." />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<title>
 		Mathieu's MakeUp Studio
 	</title>
 
-	<link rel="shortcut icon" type="image/png" href="img/lipstick.png"/>
-	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"/>
-	<link rel="stylesheet" href="css/main.css"/>
+	<link rel="shortcut icon" type="image/png" href="img/lipstick.png" />
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
+	<link rel="stylesheet" href="css/main.css" />
 </head>
 
 <body>
 
 	<nav class="w3-bar">
-		<a class="w3-bar-item w3-button w3-mobile w3-hide-medium w3-hide-large">&equiv;</a><!-- In den Unterlage &#2261, funktioniert aber in utf8 nicht!, mit &#8801 würde es gehen, aber die html-identity gefällt mir am besten. -->
+		<!-- In den Unterlage &#2261, funktioniert aber in utf8 nicht!, mit &#8801 würde es gehen, aber die html-identity gefällt mir am besten. -->
+		<a class="w3-bar-item w3-button w3-mobile w3-hide-medium w3-hide-large">&equiv;</a>
 		<a href="./index.html#home" class="w3-bar-item w3-button w3-mobile w3-hide-small">Home</a>
 		<a href="./index.html#information" class="w3-bar-item w3-button w3-mobile w3-hide-small">Informationen</a>
 		<a href="./index.html#draw" class="w3-bar-item w3-button w3-mobile w3-hide-small">Zeichnen!</a>
