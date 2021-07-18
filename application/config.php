6d97df14b1de804f18345516eafff02acbb63f64
Mathieu Lichtsteiner <Mathieu-Lichtsteiner@users.noreply.github.com>
2021-07-18 18:20:56 +0200

    setup the necessary global variables.

---
+++ application/config.php
@@ -0,0 +1,12 @@
+<?php
+define("CORE", APP . "core" . SLASH);
+define("CONTROLLER", APP . "controller" . SLASH);
+define("MODEL", APP . "model" . SLASH);
+define("VIEW", APP . "view" . SLASH);
+define("DATA", APP . "data" . SLASH);
+
+// Database configuration
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_NAME", "portofolio");
--- public/index.php
+++ public/index.php
@@ -2,3 +2,7 @@
 define("SLASH", DIRECTORY_SEPARATOR);
 define("ROOT", dirname(__DIR__) . SLASH);
 define("APP", ROOT . "application" . SLASH);
+define("CONFIG_FILE", APP . "config.php");
+
+// Always have the configuration-file loaded
+require CONFIG_FILE;
